67f18ab47f5af9a3707ba977d963ec1cae14d29b
Jordan61077 <Jordan61077@GB-LD1LPW10G017.mshome.net>
2019-06-13 11:00:30 +0100

    implemented service methods

--- Application/sampleWebApp/src/main/java/com/addresslookup/service/impl/AddressServiceImpl.java
+++ Application/sampleWebApp/src/main/java/com/addresslookup/service/impl/AddressServiceImpl.java
@@ -32,5 +32,29 @@ public class AddressServiceImpl implements AddressService{
 		return addressDao.findAll();
 	}
 
+	@Override
+	public Address readAll(Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address readJsonFromUrl(Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address postcodeRequest(Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address postcodeAndHouseRequest(Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
