ae8e83eec35c62c116e75ce768e1ae6b1add3948
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-06-25 13:42:26 +0100

    pushing changes to addressDAOImpl so same format as Loqate

--- Application/sampleWebApp/src/main/java/com/addresslookup/dao/AddressDAOImpl.java
+++ Application/sampleWebApp/src/main/java/com/addresslookup/dao/AddressDAOImpl.java
@@ -7,8 +7,6 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
