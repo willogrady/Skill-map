71f63f1e347da85fd2cdce5b163b01e05efbe8dd
Jordan61077 <Jordan61077@GB-LD1LPW10G017.mshome.net>
2019-06-25 13:04:17 +0100

    return json object, for loqate, correct format

--- Application/sampleWebApp/src/main/java/com/addresslookup/dao/LoqateDAO.java
+++ Application/sampleWebApp/src/main/java/com/addresslookup/dao/LoqateDAO.java
@@ -1,6 +1,7 @@
 package com.addresslookup.dao;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 import org.json.JSONException;
@@ -10,7 +11,7 @@ import com.addresslookup.entity.LoqateBean;
 
 public interface LoqateDAO {
 	
-	public List<String> findFirst(LoqateBean loqateBean) throws IOException, JSONException;
+	public JSONObject findFirst(LoqateBean loqateBean) throws IOException, JSONException;
 	public JSONObject retrieve(LoqateBean loqateBean) throws Exception;
 
 }
