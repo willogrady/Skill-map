914d5788c119499c8a51fb7496de4498b275a6e8
Eleanor61082 <Eleanor61082@GB-LD1LPW10G005.mshome.net>
2019-07-17 10:25:53 +0100

    Adding Spring Boot Application Role

---
+++ role-app/src/test/java/com/skillsmap/mastekeer/MastekeerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.skillsmap.mastekeer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+public class MastekeerApplicationTests {
+
+    @Test
+    public void testSample() {
+        assertEquals(true, true);
+    }
+}
