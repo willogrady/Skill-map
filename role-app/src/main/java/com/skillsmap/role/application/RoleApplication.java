729dbbc6e711f1a4b84c98bcc8a685855b951f3a
Eleanor61082 <Eleanor61082@GB-LD1LPW10G005.mshome.net>
2019-07-23 15:23:24 +0100

    changing spring application

--- role-app/src/main/java/com/skillsmap/role/application/RoleApplication.java
+++ role-app/src/main/java/com/skillsmap/role/application/RoleApplication.java
@@ -1,11 +1,18 @@
 package com.skillsmap.role.application;
 
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class RoleApplication {
@@ -18,16 +25,31 @@ public class RoleApplication {
 		ConfigurableApplicationContext ctx = 
 		SpringApplication.run(RoleApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect these fine tasty beans provided by Spring Boot.......:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
 	
+    }
+    
 	@Controller
 	class WebController {
 		
-		@GetMapping
+		@RequestMapping("/")
+		@ResponseBody
 		public String home(){
-			return "index";
+			return "hello world";
 		}
 	}
-	
-	
 
 }
