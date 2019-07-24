a586843b37901eeca7f7b802b1feffa67cb61039
Eleanor61082 <Eleanor61082@GB-LD1LPW10G005.mshome.net>
2019-07-24 09:50:48 +0100

    change to application class, pom and moved the index file to web-inf

--- role-app/src/main/java/com/skillsmap/role/application/RoleApplication.java
+++ role-app/src/main/java/com/skillsmap/role/application/RoleApplication.java
@@ -6,17 +6,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
-public class RoleApplication {
+public class RoleApplication extends SpringBootServletInitializer {
 
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(RoleApplication.class);
 	}
