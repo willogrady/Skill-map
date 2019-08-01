00b42ccbc6a552d5dde5f4a50ddf20a277a68de9
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-08-01 13:44:40 +0100

    added packages, edited application properties and began entity

---
+++ role-app/src/main/java/com/skillsmap/role/application/entity/Role.java
@@ -0,0 +1,12 @@
+package com.skillsmap.role.application.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@Table(name="roles")
+@XmlRootElement
+public class Role {
+
+}
