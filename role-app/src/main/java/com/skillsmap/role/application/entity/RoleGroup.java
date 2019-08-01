0e19e758d61a84f9a3059cef97862c8f449550a8
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-08-01 16:37:21 +0100

    fixed error with roles parameter in RoleGroup entity

--- role-app/src/main/java/com/skillsmap/role/application/entity/RoleGroup.java
+++ role-app/src/main/java/com/skillsmap/role/application/entity/RoleGroup.java
@@ -10,36 +10,28 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.ws.rs.FormParam;
 
 @Entity
+@Table(name="role_group")
 public class RoleGroup {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@FormParam("role_group_id")
 	public int role_group_id;
 	
+	@FormParam("role_group")
 	public String role_group;
 	
-	Set<Role> roles = new HashSet<>();
-	
-	@OneToMany(mappedBy="role_group", cascade=CascadeType.ALL,fetch=FetchType.EAGER)
-	public Set<Role> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
-
-	@Override
-	public String toString() {
-		return "RoleGroup [role_group_id=" + role_group_id + ", role_group=" + role_group + "]";
-	}
+	@OneToMany(mappedBy="roleGroup")
+	private Set<Role> roles;
 
 	public int getRole_group_id() {
 		return role_group_id;
 	}
-	
+
 	public void setRole_group_id(int role_group_id) {
 		this.role_group_id = role_group_id;
 	}
@@ -51,7 +43,19 @@ public class RoleGroup {
 	public void setRole_group(String role_group) {
 		this.role_group = role_group;
 	}
-	
-	
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	@Override
+	public String toString() {
+		return "RoleGroup [role_group_id=" + role_group_id + ", role_group=" + role_group + ", roles=" + roles + "]";
+	}
 	
 }
+	
