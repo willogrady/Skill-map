46d99341e35facab3ae72fcccae36800e6d16816
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-08-01 14:02:35 +0100

    commit to pull

--- role-app/src/main/java/com/skillsmap/role/application/entity/Role.java
+++ role-app/src/main/java/com/skillsmap/role/application/entity/Role.java
@@ -1,12 +1,92 @@
 package com.skillsmap.role.application.entity;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.ws.rs.FormParam;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @Table(name="roles")
 @XmlRootElement
 public class Role {
+	
+	@FormParam("RoleID")
+	public int role_id;
+	
+	@FormParam("RoleTitle")
+	public String role_title;
+	
+	@FormParam("RoleGrad")
+	public String role_grade;
+	
+	@FormParam("RoleSummary")
+	public String role_summary;
+	
+	RoleGroup roleGroup;
+	
+	@ManyToOne
+	@JoinColumn(name="FK_role_group_id")
+	public RoleGroup getRoleGroup() {
+		return roleGroup;
+	}
+	
+	public void setRoleGroup(RoleGroup roleGroup) {
+		this.roleGroup = roleGroup;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [role_id=" + role_id + ", role_title=" + role_title + ", role_grade=" + role_grade
+				+ ", role_summary=" + role_summary + "]";
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+
+	public String getRole_title() {
+		return role_title;
+	}
+
+	public void setRole_title(String role_title) {
+		this.role_title = role_title;
+	}
+
+	public String getRole_grade() {
+		return role_grade;
+	}
+
+	public void setRole_grade(String role_grade) {
+		this.role_grade = role_grade;
+	}
+
+	public String getRole_summary() {
+		return role_summary;
+	}
+
+	public void setRole_summary(String role_summary) {
+		this.role_summary = role_summary;
+	}
+	
+	
+	
+	
+	
+	
 
 }
---
+++ role-app/src/main/java/com/skillsmap/role/application/entity/RoleGroup.java
@@ -0,0 +1,51 @@
+package com.skillsmap.role.application.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RoleGroup {
+
+	public int role_group_id;
+	
+	public String role_group;
+	
+	public int version_id;
+
+	@Override
+	public String toString() {
+		return "RoleGroup [role_group_id=" + role_group_id + ", role_group=" + role_group + ", version_id=" + version_id
+				+ "]";
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int getRole_group_id() {
+		return role_group_id;
+	}
+
+	public void setRole_group_id(int role_group_id) {
+		this.role_group_id = role_group_id;
+	}
+
+	public String getRole_group() {
+		return role_group;
+	}
+
+	public void setRole_group(String role_group) {
+		this.role_group = role_group;
+	}
+
+	public int getVersion_id() {
+		return version_id;
+	}
+
+	public void setVersion_id(int version_id) {
+		this.version_id = version_id;
+	}
+	
+	
+	
+}
