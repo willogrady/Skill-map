d5ef9472fd6975d0d6fb775ff59f3bf2df2b56b8
scottfreeman1996 <Scott61079@GB-LD1LPW10G008.mastek.com>
2019-09-16 11:32:54 +0100

    comment to push

--- role-app/src/main/java/com/skillsmap/role/application/controller/RoleController.java
+++ role-app/src/main/java/com/skillsmap/role/application/controller/RoleController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.skillsmap.role.application.entity.Role;
 import com.skillsmap.role.application.repository.RoleRepository;
-
+//
 @RestController
 @Component
 @CrossOrigin(origins = "http://localhost:4200")
